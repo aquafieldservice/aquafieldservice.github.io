2101ff80bfdd59cf8d144efa3cbe4920af63bb2a
Chuck Konkol <ckonkol@gmail.com>
2024-02-12 10:52:31 -0600

    cc

---
+++ precache-manifest.9611af656240e084317119bdf5473c3b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "75e8ebfe5b68ff9723c5",
+    "url": "/static/css/main.d9f5c236.chunk.css"
+  },
+  {
+    "revision": "75e8ebfe5b68ff9723c5",
+    "url": "/static/js/main.c64af2ea.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0a0f98c6d276cb90fef6",
+    "url": "/static/js/2.727bc9e2.chunk.js"
+  },
+  {
+    "revision": "d287bb2ac8d832c8d8a293562266cc7a",
+    "url": "/index.html"
+  }
+];
